ad794e67029f17c5205af37a7ad7f00932b5887d
Vladimir Azanza <vladimirazanza@gmail.com>
2024-03-09 15:01:13 +0500

    Apply screen edge processing

--- the_snake.py
+++ the_snake.py
@@ -98,23 +98,33 @@ class Snake(GameObject):
     def update_direction(self):
         """Updates the direction of snake"""
         if self.next_direction:
+            # Actualiza la direccion segun la tecla apretada
             self.direction = self.next_direction
+            # Actualiza la direccion a none para que mantenga su curso
             self.next_direction = None
 
     def move(self):
         """Updates the position of snake"""
         """Add new head and remove last element if length has not increased"""
         self.update_direction()
-        self.head_x, self.head_y = self.get_head_position()
+        head_x, head_y = self.get_head_position()
 
-        new_head_x = self.head_x + self.direction[0] * GRID_SIZE
-        new_head_y = self.head_y + self.direction[1] * GRID_SIZE
-
+        new_head_x = head_x + self.direction[0] * GRID_SIZE
+        new_head_y = head_y + self.direction[1] * GRID_SIZE
         self.positions.insert(0, (new_head_x, new_head_y))
 
         if len(self.positions) > self.length:
-            self.last = self.positions.pop(-1)
-            # self.last = (new_head_x, new_head_y)
+            self.last = (head_x, head_y)
+            self.positions.pop(-1)
+
+        if new_head_x < 0:
+            self.positions[0] = (SCREEN_WIDTH - GRID_SIZE, head_y)
+        elif new_head_x >= SCREEN_WIDTH:
+            self.positions[0] = (0, head_y)
+        elif new_head_y < 0:
+            self.positions[0] = (head_x, SCREEN_HEIGHT - GRID_SIZE)
+        elif new_head_y >= SCREEN_HEIGHT:
+            self.positions[0] = (head_x, 0)
 
     def draw(self, surface):
         """Draws the snake"""
@@ -176,64 +186,9 @@ def main():
         snake.move()
         snake.draw(screen)
         handle_keys(snake)
-
-        # snake.draw(screen)
+        
         pygame.display.update()
 
 
 if __name__ == '__main__':
     main()
-
-
-# Метод draw класса Apple
-# def draw(self, surface):
-#     rect = pygame.Rect(
-#         (self.position[0], self.position[1]),
-#         (GRID_SIZE, GRID_SIZE)
-#     )
-#     pygame.draw.rect(surface, self.body_color, rect)
-#     pygame.draw.rect(surface, BORDER_COLOR, rect, 1)
-
-# # Метод draw класса Snake
-# def draw(self, surface):
-#     for position in self.positions[:-1]:
-#         rect = (
-#             pygame.Rect((position[0], position[1]), (GRID_SIZE, GRID_SIZE))
-#         )
-#         pygame.draw.rect(surface, self.body_color, rect)
-#         pygame.draw.rect(surface, BORDER_COLOR, rect, 1)
-
-#     # Отрисовка головы змейки
-#     head_rect = pygame.Rect(self.positions[0], (GRID_SIZE, GRID_SIZE))
-#     pygame.draw.rect(surface, self.body_color, head_rect)
-#     pygame.draw.rect(surface, BORDER_COLOR, head_rect, 1)
-
-#     # Затирание последнего сегмента
-#     if self.last:
-#         last_rect = pygame.Rect(
-#             (self.last[0], self.last[1]),
-#             (GRID_SIZE, GRID_SIZE)
-#         )
-#         pygame.draw.rect(surface, BOARD_BACKGROUND_COLOR, last_rect)
-
-# Функция обработки действий пользователя
-# def handle_keys(game_object):
-#     for event in pygame.event.get():
-#         if event.type == pygame.QUIT:
-#             pygame.quit()
-#             raise SystemExit
-#         elif event.type == pygame.KEYDOWN:
-#             if event.key == pygame.K_UP and game_object.direction != DOWN:
-#                 game_object.next_direction = UP
-#             elif event.key == pygame.K_DOWN and game_object.direction != UP:
-#                 game_object.next_direction = DOWN
-#             elif event.key == pygame.K_LEFT and game_object.direction != RIGHT:
-#                 game_object.next_direction = LEFT
-#             elif event.key == pygame.K_RIGHT and game_object.direction != LEFT:
-#                 game_object.next_direction = RIGHT
-
-# Метод обновления направления после нажатия на кнопку
-# def update_direction(self):
-#     if self.next_direction:
-#         self.direction = self.next_direction
-#         self.next_direction = None
